5c715bab03bfafd8266f835d7a8c6ec036576e8d
Production Server <Billy@W-7U-D-00.(none)>
2013-07-26 20:41:00 -0400

    Added User migration.

---
+++ app/database/migrations/2013_07_27_000025_create_user_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone', 10);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('street_address', 40);
+            $table->string('city', 45);
+            $table->string('state', 2);
+            $table->string('zip_code', 6);
+            $table->string('last_logged_in_from', 12);
+            $table->timestamp('last_logged_in_at');
+            $table->boolean('stay_logged_in');
+            $table->boolean('validated');
+            $table->integer('permissions');
+            $table->boolean('active');
+            $table->timestamp('deleted_at');
+            $table->timestamps();    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user');
+    }
+
+}
