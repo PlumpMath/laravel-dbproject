9e82e6e5df37bd505decfdcefde0bb69031eaef8
William Stein <williamstein92@gmail.com>
2013-08-02 01:29:42 -0400

    Added JS, partials to CSS and created master view.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <!--[if IE 7 ]>    <html class="no-js ie7"> <![endif]-->
+    <!--[if (gt IE 7)|!(IE)]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>{{ $title }}</title>
+        <meta name="description" content="">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="cleartype" content="on">
+        {{ HTML::style('css/main.css') }} 
+        {{ HTML::script('js/vendor/modernizr-2.6.2.min.js') }}
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+
+        {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
+        <script>window.jQuery || document.write('{{ HTML::script('js/vendor/jquery-2.0.3.min.js' }}')</script>
+        {{ HTML::script('js/helper.js') }}
+        {{ HTML::script('js/main.js') }}
+    </body>
+</html>
