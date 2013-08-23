09bd5bc253730933ec181008ff0e4536e2984879
William Stein <williamstein92@gmail.com>
2013-08-23 03:33:35 -0400

    Locations show. Check out /locations/4

--- app/views/location.blade.php
+++ app/views/location.blade.php
@@ -16,7 +16,25 @@
       <i class='icon-trash'></i>
     </div>
   </div>
-  <div class='container'>
+  <div class='sidebar'>
+    <div class='cell'>
+      <div class='account_info'>
+        <div class='user'>Admin</div>
+        <div class='user_label'>account name</div>
+        <div class='signout'>
+          <i class='icon-signout'></i>
+          <div class='signout_label'>Sign Out</div>
+        </div>
+        <div class='signout'>
+          <i class='icon-cogs'></i>
+          <div class='signout_label'>Account Settings</div>
+        </div>
+      </div>
+    </div>
+    <div class='nav'>
+    </div>
+  </div>
+  <div class='table'>
     <div class='cell'>
       <div class='content'>
         <div class='title'>Melville Sports Center</div>
---
+++ app/views/location_full.blade.php
@@ -0,0 +1,82 @@
+<head>
+  {{ HTML::style('css/main.css') }}
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+  <style>
+  </style>
+</head>
+<body>
+  <div class='sidebar'>
+    <div class='commands'>
+      <div class='copy icon'>
+        <i class='icon-copy'></i>
+      </div>
+      <div class='edit icon'>
+        <i class='icon-edit'></i>
+      </div>
+      <div class='delete icon'>
+        <i class='icon-trash'></i>
+      </div>
+    </div>
+    <div class='account_info'>
+      <div class='user'>Admin</div>
+      <div class='user_label'>account name</div>
+      <div class='signout'>
+        <i class='icon-signout'></i>
+        <div class='signout_label'>Sign Out</div>
+      </div>
+      <div class='signout'>
+        <i class='icon-cogs'></i>
+        <div class='signout_label'>Account Settings</div>
+      </div>
+    </div>
+    <div class='nav'>
+    </div>
+  </div>
+  <div class='table'>
+    <div class='content'>
+      <div class='title'>{{ $rows['Name'] }}</div>
+      <div class='address'>{{ $rows['Address'].', '.$rows['City'].', '.$rows['State'].' '.$rows['Zip Code'] }} </div>
+      <h1>Location Details</h1>
+      <table>
+        <tbody>
+          @foreach ($rows as $key =>  $row)
+          <tr>
+            <td>{{ $key }}</td>
+            <td>{{ $row }}</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+      <h1>Enrollment Details</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Number of Sessions</td><td>0</td>
+          </tr>
+          <tr>
+            <td>Current Enrollment</td><td>0 students</td>
+          </tr>
+          <tr>
+            <td>Total Enrollment</td><td>0 students</td>
+          </tr>
+          <tr>
+            <td>Average Enrollment</td><td>0 students</td>
+          </tr>
+          <tr>
+            <td>Current Revenue</td><td>$0</td>
+          </tr>
+          <tr>
+            <td>Total Revenue</td><td>$0</td>
+          </tr>      
+        </tbody>
+      </table>
+      <h1>Tasks</h1>
+      <a href='#'>Fetch related Students</a>
+      <a href='#'>Fetch related Sessions</a>
+    </div>
+  </div>
+  <div class='name'>
+    myafterschoolprograms, inc.
+  </div>
+</body>
+</html>
